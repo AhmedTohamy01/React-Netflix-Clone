d389e7667022d42d70bf0543b4e835668d30237e
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-09-01 00:53:19 +0200

    Build OptFormButton Component

---
+++ src/components/OptForm/OptFormButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function optFormButton({children, ...restProps}) {
+  return (
+    <div className="optform-button" {...restProps}>
+      {children}
+      <img className="optform-button-image" src="/images/icons/chevron-right.png" alt="Try Now" />
+    </div>
+  )
+}
+
+export default optFormButton;
