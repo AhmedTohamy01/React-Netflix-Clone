c50e0138a138eb6b5c94a01751fb1d975b9b0643
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-09-01 00:45:36 +0200

    Build OptFormText Component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import HeaderCompound from "../compounds/HeaderCompound";
+import OptFormCompound from "../compounds/OptFormCompound";
 
 function HomePage() {
   return (
