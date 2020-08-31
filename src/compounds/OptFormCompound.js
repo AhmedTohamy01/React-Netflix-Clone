14f7c462c1299f6e0288f9d5cce62f34e640520b
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-09-01 00:49:38 +0200

    Add OptFormText to OptFormCompound

--- src/compounds/OptFormCompound.js
+++ src/compounds/OptFormCompound.js
@@ -1,9 +1,11 @@
 import React from "react";
 import OptFormWrapper from "../components/OptForm/OptFormWrapper";
+import OptFormText from "../components/OptForm/OptFormText";
 
 function OptFormCompound() {
   return (
     <>
+    <OptFormText>Ready to watch? Enter your email to create or restart your membership.</OptFormText>
     <OptFormWrapper>
     </OptFormWrapper>
     </>
