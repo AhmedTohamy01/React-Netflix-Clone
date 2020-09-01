c57caab6523869f1a21a40ba42dc99059ff762a4
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-09-01 18:16:56 +0200

    Add Seperator to HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,7 @@ import React from "react";
 import HeaderCompound from "../compounds/HeaderCompound";
 import OptFormCompound from "../compounds/OptFormCompound";
 import JumboCompound from "../compounds/JumboCompound";
+import Seperator from "../components/Seperator/Seperator";
 
 function HomePage() {
   return (
@@ -9,6 +10,7 @@ function HomePage() {
     <HeaderCompound>
       <OptFormCompound />
     </HeaderCompound>
+    <Seperator />
     <JumboCompound />
     </>
   )
