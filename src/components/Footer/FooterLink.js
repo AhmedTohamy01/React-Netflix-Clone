9a27330891ceb31a5285c94d891950ac85f9d9cb
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-09-02 01:46:35 +0200

    Build FooterLink Component & Add it to FooterCompound

---
+++ src/components/Footer/FooterLink.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function FooterLink({children, ...restProps}) {
+  return (
+  <a href="#" className="footer-link" {...restProps}>{children}</a>
+  )
+}
+
+
+export default FooterLink;
