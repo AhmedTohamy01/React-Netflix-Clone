aa51475f9480572adaf63e8f689816b066515466
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-09-02 01:36:39 +0200

    Build FooterColumn Component & Add it to FooterCompound

--- src/compounds/FooterCompound.js
+++ src/compounds/FooterCompound.js
@@ -2,12 +2,16 @@ import React from "react";
 import FooterWrapper from "../components/Footer/FooterWrapper";
 import FooterTitle from "../components/Footer/FooterTitle";
 import FooterRow from "../components/Footer/FooterRow";
+import FooterColumn from "../components/Footer/FooterColumn";
+
 
 function FooterCompound() {
   return (
     <FooterWrapper>
       <FooterTitle>Questions? Contact us.</FooterTitle>
-      <FooterRow></FooterRow>
+      <FooterRow>
+        <FooterColumn></FooterColumn>
+      </FooterRow>
     </FooterWrapper>
   )
 }
