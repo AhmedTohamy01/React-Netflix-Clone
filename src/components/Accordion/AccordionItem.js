94d757998237625ea07047242f81fe8de0aeaba4
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-09-01 21:18:10 +0200

    Build AccordionItem Component & Add it to AccordionCompound

---
+++ src/components/Accordion/AccordionItem.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function AccordionItem({children, ...restProps}) {
+  return (
+    <>
+    </>
+  )
+}
+
+export default AccordionItem
--- src/compounds/AccordionCompound.js
+++ src/compounds/AccordionCompound.js
@@ -1,11 +1,15 @@
 import React from "react";
 import AccordionWrapper from "../components/Accordion/AccordionWrapper";
 import AccordionTitle from "../components/Accordion/AccordionTitle";
+import AccordionItem from "../components/Accordion/AccordionItem";
 
 function AccordionCompound() {
   return (
     <AccordionWrapper>
       <AccordionTitle>Frequently Asked Questions</AccordionTitle>
+      <AccordionItem>
+
+      </AccordionItem>
     </AccordionWrapper>
   )
 }
