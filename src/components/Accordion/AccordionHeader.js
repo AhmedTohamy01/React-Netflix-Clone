a07f0b5a507ec6386b12d479c45fca84c16f2945
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-09-01 21:23:35 +0200

    Build AccordionHeader Component & Add it to AccordionCompound

--- src/components/Accordion/AccordionHeader.js
+++ src/components/Accordion/AccordionHeader.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-function AccordionItem({children, ...restProps}) {
+
+function AccordionHeader({children, ...restProps}) {
+
   return (
     <div {...restProps}>{children}</div>
   )
 }
 
-export default AccordionItem
+export default AccordionHeader
--- src/compounds/AccordionCompound.js
+++ src/compounds/AccordionCompound.js
@@ -2,12 +2,14 @@ import React from "react";
 import AccordionWrapper from "../components/Accordion/AccordionWrapper";
 import AccordionTitle from "../components/Accordion/AccordionTitle";
 import AccordionItem from "../components/Accordion/AccordionItem";
+import AccordionHeader from "../components/Accordion/AccordionHeader";
 
 function AccordionCompound() {
   return (
     <AccordionWrapper>
       <AccordionTitle>Frequently Asked Questions</AccordionTitle>
       <AccordionItem>
+        <AccordionHeader></AccordionHeader>
 
       </AccordionItem>
     </AccordionWrapper>
