089831f3a5e7afd9f7ad00994e2085c740e357b8
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-09-01 16:03:24 +0200

    Build JumboCompound Component & Add it to HomePage

---
+++ src/compounds/JumboCompound.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function JumboCompound() {
+  return (
+        <>
+        </>
+  )
+}
+
+export default JumboCompound 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,16 @@
 import React from "react";
 import HeaderCompound from "../compounds/HeaderCompound";
 import OptFormCompound from "../compounds/OptFormCompound";
+import JumboCompound from "../compounds/JumboCompound";
 
 function HomePage() {
   return (
+    <>
     <HeaderCompound>
       <OptFormCompound />
     </HeaderCompound>
+    <JumboCompound />
+    </>
   )
 }
 
