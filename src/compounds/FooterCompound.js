d0eccf84409a32672f798b7353d1710ab53b6409
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-09-02 01:30:09 +0200

    Build FooterTitle Component & Add it to FooterCompound

--- src/compounds/FooterCompound.js
+++ src/compounds/FooterCompound.js
@@ -1,10 +1,11 @@
 import React from "react";
 import FooterWrapper from "../components/Footer/FooterWrapper";
+import FooterTitle from "../components/Footer/FooterTitle";
 
 function FooterCompound() {
   return (
     <FooterWrapper>
-
+      <FooterTitle>Questions? Contact us.</FooterTitle>
     </FooterWrapper>
   )
 }
