c7e883806d61e657e015c680ab769358cc5d2349
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-09-01 23:29:03 +0200

    Map through FAQs JSON file and get Accordion data from it

--- src/compounds/AccordionCompound.js
+++ src/compounds/AccordionCompound.js
@@ -4,21 +4,18 @@ import AccordionTitle from "../components/Accordion/AccordionTitle";
 import AccordionItem from "../components/Accordion/AccordionItem";
 import AccordionHeader from "../components/Accordion/AccordionHeader";
 import AccordionBody from "../components/Accordion/AccordionBody";
+import FAQData from "../data/faqs.json";
 
 function AccordionCompound() {
   return (
     <AccordionWrapper>
       <AccordionTitle>Frequently Asked Questions</AccordionTitle>
-      <AccordionItem>
-        <AccordionHeader>What is Netflix?</AccordionHeader>
-        <AccordionBody>
-        Netflix is a streaming service that offers a wide variety of award-winning TV programmes, 
-        films, anime, documentaries and more – on thousands of internet-connected devices.\n\nYou 
-        can watch as much as you want, whenever you want, without a single advert – all for one low 
-        monthly price. There's always something new to discover, and new TV programmes and films are 
-        added every week!
-        </AccordionBody>
-      </AccordionItem>
+      {FAQData.map((item) => (
+        <AccordionItem key={item.id}>
+          <AccordionHeader>{item.header}</AccordionHeader>
+          <AccordionBody>{item.body}</AccordionBody>
+        </AccordionItem>
+      ))}
     </AccordionWrapper>
   )
 }
