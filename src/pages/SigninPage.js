e4a387fc5a58ae30dea24b1f82356c4e8e17d1e9
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-09-03 15:43:48 +0200

    Add NavBar to SigninPage

--- src/pages/SigninPage.js
+++ src/pages/SigninPage.js
@@ -1,11 +1,15 @@
 import React from "react";
+import HeaderWrapper from "../components/Header/HeaderWrapper";
+import NavBar from "../components/Header/NavBar";
 
 function SigninPage() {
 
   return (
     <>
     <HeaderWrapper>
-      
+      <NavBar>
+
+      </NavBar>
     </HeaderWrapper>
     </>
   )
