1a3cf26bf6b2eab210fd164560f7928aee56ba16
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-09-04 00:15:21 +0200

    Copy SigninPage, Rename it to SignupPage and Add it to App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import SigninPage from './pages/SigninPage';
+import SignupPage from './pages/SignupPage';
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
         </Route>
         <Route path="/signin">
           <SigninPage />
+        </Route>
+        <Route path="/signup">
+          <SignupPage />
         </Route>
       </Switch>
     </Router>
