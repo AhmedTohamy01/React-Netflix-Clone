b15a5f518c78f8b16fb33e8bbf59104ae03f633a
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-09-04 16:42:18 +0200

    Add HeaderWrapper to BrowsePage & Style it

--- src/pages/BrowsePage.js
+++ src/pages/BrowsePage.js
@@ -1,11 +1,14 @@
 import React from "react";
 import HeaderWrapper from "../components/Header/HeaderWrapper";
+import NavBar from "../components/Header/NavBar";
 
 function BrowsePage() {
 
   return (
     <>
     <HeaderWrapper className="header-wrapper-browse">
+      <NavBar className="navbar-browse">
+      </NavBar>
     </HeaderWrapper>
     </>
   )
