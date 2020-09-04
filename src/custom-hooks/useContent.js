cd8b91a1d3608a5d2ef23b4bf4bad63a0a93ae01
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-09-04 23:06:42 +0200

    Build useContent Custom Hook to get movies  data from firebase

---
+++ src/custom-hooks/useContent.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState, useContext } from 'react';
+import { FirebaseContext } from "../context/FirbaseContext";
+
+
+function useContent(target) {
+  const [content, setContent] = useState([]);
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+        firebase
+            .firestore()
+            .collection(target)
+            .get()
+            .then((snapshot) => {
+                const allContent = snapshot.docs.map((contentObj) => ({
+                    ...contentObj.data(),
+                    docId: contentObj.id,
+                }));
+                
+                setContent(allContent);
+            })
+            .catch((error) => {
+                console.log(error.message);
+            });
+    }, [])
+
+  return (
+    { [target]: content }
+  )
+}
+
+export default useContent;
