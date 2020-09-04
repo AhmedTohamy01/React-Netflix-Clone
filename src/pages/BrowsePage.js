3cacfe7b148b158cdfd26db4520c7b0b099dd4f1
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-09-04 22:10:44 +0200

    Add FeaturTitle to BrowsePage & Style it

--- src/pages/BrowsePage.js
+++ src/pages/BrowsePage.js
@@ -3,6 +3,7 @@ import HeaderWrapper from "../components/Header/HeaderWrapper";
 import NavBar from "../components/Header/NavBar";
 import Logo from "../components/Header/Logo";
 import FeatureWrapper from "../components/Header/FeatureWrapper";
+import FeatureTitle from "../components/Header/FeatureTitle";
 
 
 function BrowsePage() {
@@ -12,10 +13,9 @@ function BrowsePage() {
     <HeaderWrapper className="header-wrapper-browse">
       <NavBar className="navbar-browse">
         <Logo />
-
       </NavBar>
       <FeatureWrapper>
-        
+        <FeatureTitle className="feature-title-browse">Watch Joker Now</FeatureTitle>
       </FeatureWrapper>
     </HeaderWrapper>
     </>
