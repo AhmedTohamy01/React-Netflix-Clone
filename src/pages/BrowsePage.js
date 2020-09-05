f91c8f2746d0bd9fdbadab6ae85c20ec03d4941c
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-09-05 04:31:22 +0200

    Fix showing card feature error in Series category > Feel-Good section

--- src/pages/BrowsePage.js
+++ src/pages/BrowsePage.js
@@ -30,7 +30,7 @@ function BrowsePage() {
      { title: 'Comedies', data: series.filter((item) => item.genre === 'comedies') },
      { title: 'Children', data: series.filter((item) => item.genre === 'children') },
      { title: 'Crime', data: series.filter((item) => item.genre === 'crime') },
-     { title: 'Feel Good', data: series.filter((item) => item.genre === 'feel-good') },
+     { title: 'Feel-Good', data: series.filter((item) => item.genre === 'feel-good') },
   ]
 
   let {films} = useContent('films');
