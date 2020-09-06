b8594440de0f3d7b3df320d582d5ebf27f26f90f
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-09-07 01:57:43 +0200

    Updates

---
+++ precache-manifest.1d9b067f96d84b29610807a9ce19e77f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cca9e3d8be058b50ebff8a9cba7d1e44",
+    "url": "/React-Netflix-Clone/index.html"
+  },
+  {
+    "revision": "042b073fa41c04ed8a22",
+    "url": "/React-Netflix-Clone/static/js/2.e1e05167.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/React-Netflix-Clone/static/js/2.e1e05167.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "185baca43672bfd35b2a",
+    "url": "/React-Netflix-Clone/static/js/main.f568b52c.chunk.js"
+  },
+  {
+    "revision": "7feec2a1fb79489307ae",
+    "url": "/React-Netflix-Clone/static/js/runtime-main.e39c0fbf.js"
+  },
+  {
+    "revision": "6dbba458959d4ce1edd2f5b3ab3ae13b",
+    "url": "/React-Netflix-Clone/static/media/logo.6dbba458.svg"
+  }
+]);
