9c2bee852950d5b1fc93050c634f4fb815c619cb
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-10-11 14:29:28 +0200

    Add Error Handling to Signin & Signup Form

--- src/pages/SigninPage.js
+++ src/pages/SigninPage.js
@@ -1,38 +1,41 @@
-import React, { useState, useContext } from 'react';
-import { useHistory } from 'react-router-dom';
-import { FirebaseContext } from '../context/FirbaseContext';
-import HeaderWrapper from '../components/Header/HeaderWrapper';
-import NavBar from '../components/Header/NavBar';
-import Logo from '../components/Header/Logo';
-import FooterCompound from '../compounds/FooterCompound';
-import SignFormWrapper from '../components/SignForm/SignFormWrapper';
-import SignFormBase from '../components/SignForm/SignFormBase';
-import SignFormTitle from '../components/SignForm/SignFormTitle';
-import SignFormInput from '../components/SignForm/SignFormInput';
-import SignFormButton from '../components/SignForm/SignFormButton';
-import SignFormText from '../components/SignForm/SignFormText';
-import SignFormLink from '../components/SignForm/SignFormLink';
-import SignFormCaptcha from '../components/SignForm/SignFormCaptcha';
+import React, { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { FirebaseContext } from "../context/FirbaseContext";
+import HeaderWrapper from "../components/Header/HeaderWrapper";
+import NavBar from "../components/Header/NavBar";
+import Logo from "../components/Header/Logo";
+import FooterCompound from "../compounds/FooterCompound";
+import SignFormWrapper from "../components/SignForm/SignFormWrapper";
+import SignFormBase from "../components/SignForm/SignFormBase";
+import SignFormTitle from "../components/SignForm/SignFormTitle";
+import SignFormInput from "../components/SignForm/SignFormInput";
+import SignFormButton from "../components/SignForm/SignFormButton";
+import SignFormText from "../components/SignForm/SignFormText";
+import SignFormLink from "../components/SignForm/SignFormLink";
+import SignFormCaptcha from "../components/SignForm/SignFormCaptcha";
 
 function SigninPage() {
   const history = useHistory();
   const { firebase } = useContext(FirebaseContext);
 
-  const [emailAddress, setEmailAddress] = useState('');
-  const [password, setPassword] = useState('');
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-  const IsInvalid = password === '' || emailAddress === '';
+  const IsInvalid = password === "" || emailAddress === "";
 
   function handleSubmit(event) {
     event.preventDefault();
 
-    firebase.auth()
+    firebase
+      .auth()
       .signInWithEmailAndPassword(emailAddress, password)
       .then(() => {
-        setEmailAddress('');
-        setPassword('');
-        history.push('./browse');
-      });
+        setEmailAddress("");
+        setPassword("");
+        history.push("./browse");
+      })
+      .catch((error) => setError(error.message));
   }
 
   return (
@@ -44,6 +47,7 @@ function SigninPage() {
         <SignFormWrapper>
           <SignFormBase onSubmit={handleSubmit} method="POST">
             <SignFormTitle>Sign In</SignFormTitle>
+            {error ? <SignFormError>{error}</SignFormError> : null}
             <SignFormInput
               type="text"
               placeholder="Email Address"
@@ -63,7 +67,8 @@ function SigninPage() {
               <SignFormLink href="./#/signup">Sign up now.</SignFormLink>
             </SignFormText>
             <SignFormCaptcha>
-              This page is protected by Google reCAPTCHA to ensure you are not a bot.
+              This page is protected by Google reCAPTCHA to ensure you are not a
+              bot.
             </SignFormCaptcha>
           </SignFormBase>
         </SignFormWrapper>
